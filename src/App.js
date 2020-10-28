a4acf40df5563161a441919dc8cc00fce7f250d3
Alvin Mantovani <alvin.ictn@gmail.com>
2020-10-28 22:55:48 +0700

    applying sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import "./assets/sass/color.scss"
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <button className="v-bg-donker v-text-light">TESTs</button>
       </header>
     </div>
   );
